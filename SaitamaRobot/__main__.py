dec8b7ef74f9f3a9e44f9d3074790abd8da5b0c5
stormshadow3000 <83730643+stormshadow3000@users.noreply.github.com>
2021-06-04 21:28:54 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -76,7 +76,7 @@ def get_readable_time(seconds: int) -> str:
 PM_START_TEXT = """
 Hi {}, my name is {}! 
 I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups. And my 2nd bot - @pirateking_robot
+Build by weebs for weebs, I specialize in managing anime and similar themed groups And my 2nd bot @pirateking_robot
 
 """
 
